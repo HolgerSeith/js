7487d61eec1f0a317c82553b5a05cc8b388993ae
Holger Seith <holger.seith@gmail.com>
2020-05-09 12:19:37 +0200

    initial

---
+++ first.js
@@ -0,0 +1,21 @@
+var number = 5; // in comment
+
+/*
+
+adst
+*/
+
+
+var myName = "Holger";
+myName = 32;
+console.log(myName);
+
+let ourName ="seith";
+const pi = 3.14;
+
+
+var ourDecimal = 15
+var otherDecimal = 6
+
+var result = ourDecimal % otherDecimal;
+console.log(result);
